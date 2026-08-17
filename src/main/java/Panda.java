288cb71ed1eb55912b0b73a1c8e88978320a5cb6
Keegan4 <keegan.gwx@gmail.com>
2026-08-17 20:26:23 +0800

    Level-0 (b) Update Panda to greet user

--- src/main/java/Panda.java
+++ src/main/java/Panda.java
@@ -1,6 +1,25 @@
 public class Panda {
+    /**
+     * Starts the Panda chatbot and displays its greeting and closing messages.
+     *  The ascii art and dividers are created by codex.
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
-        String banner = "Hi I'm Panda";
+        String divider = "____________________________________________________________";
+        String banner = """
+                 ____    _    _   _ ____    _
+                |  _ \\  / \\  | \\ | |  _ \\  / \\
+                | |_) |/ _ \\ |  \\| | | | |/ _ \\
+                |  __// ___ \\| |\\  | |_| / ___ \\
+                |_|  /_/   \\_\\_| \\_|____/_/   \\_\\
+                """;
+
+        System.out.println(divider);
         System.out.println(banner);
+        System.out.println("Hello! I'm Panda.");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
     }
 }
